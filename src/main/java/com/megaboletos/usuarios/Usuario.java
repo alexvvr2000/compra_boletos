5ab7451c042342af6f004dbd5da12d5c9fa4add1
Alejandro Valenzuela <alejandrovalenzuela051@gmail.com>
2023-05-07 17:06:29 -0600

    agregado s y g

--- src/main/java/com/megaboletos/usuarios/Usuario.java
+++ src/main/java/com/megaboletos/usuarios/Usuario.java
@@ -1,4 +1,32 @@
 package com.megaboletos.usuarios;
 
 public class Usuario {
+    private String nombre;
+    private String apellidoPaterno;
+    private String apellidoMaterno;
+    private String correo;
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public String getApellidoPaterno() {
+        return apellidoPaterno;
+    }
+    public void setApellidoPaterno(String apellidoPaterno) {
+        this.apellidoPaterno = apellidoPaterno;
+    }
+    public String getApellidoMaterno() {
+        return apellidoMaterno;
+    }
+    public void setApellidoMaterno(String apellidoMaterno) {
+        this.apellidoMaterno = apellidoMaterno;
+    }
+    public String getCorreo() {
+        return correo;
+    }
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
 }
