58cc40d6ae9a894a2f7889ad79910fb4f28dc3e1
Alejandro Valenzuela <alejandrovalenzuela051@gmail.com>
2023-05-08 00:23:49 -0600

    implementada permiso admin

--- src/main/java/com/megaboletos/usuarios/builders/Administrador.java
+++ src/main/java/com/megaboletos/usuarios/builders/Administrador.java
@@ -1,8 +1,13 @@
 package com.megaboletos.usuarios.builders;
+import com.megaboletos.usuarios.PermisoUsuario;
 import com.megaboletos.usuarios.builders.Usuario;
 import java.sql.Connection;
-public class Administrador extends Usuario{
+public class Administrador extends Usuario implements PermisoUsuario{
     Administrador(Connection conexion, String correo, String claveAcceso){
         super(conexion, correo, claveAcceso);
+    }
+    @Override
+    public Permiso obtenerPermisosUsuario() {
+        return Permiso.ADMIN;
     }
 }
