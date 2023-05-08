5adad16250a0a1059515a5ce0212e11f84d96057
Alejandro Valenzuela <alejandrovalenzuela051@gmail.com>
2023-05-07 21:23:00 -0600

    cambiado cliente a archivo de builder 2

--- src/main/java/com/megaboletos/usuarios/builders/ClienteBuilder.java
+++ src/main/java/com/megaboletos/usuarios/builders/ClienteBuilder.java
@@ -1,4 +1,19 @@
 package com.megaboletos.usuarios.builders;
-
+import com.megaboletos.usuarios.PermisoUsuario;
+import java.sql.Connection;
 public class ClienteBuilder {
+}
+class Cliente extends Usuario implements PermisoUsuario {
+    Cliente(Connection conexion, String correo, String claveAcceso) {
+        super(conexion, correo, claveAcceso);
+    }
+    public boolean modificarMetodoPago(int idMetodoPago) {
+        return false;
+    }
+    public boolean eliminarMetodoPago(int idMetodoPago) {
+        return false;
+    }
+    public PermisoUsuario.Permiso obtenerPermisosUsuario() {
+        return Permiso.CLIENTE;
+    }
 }
