9457078bfb9ee78b5f34e7dd40e14407c07093d0
Alejandro Valenzuela <alejandrovalenzuela051@gmail.com>
2023-05-08 01:45:52 -0600

    creada base de compra

--- src/main/java/com/megaboletos/usuarios/builders/Compra.java
+++ src/main/java/com/megaboletos/usuarios/builders/Compra.java
@@ -1,3 +1,27 @@
 package com.megaboletos.usuarios.builders;
+import java.util.ArrayList;
+import java.util.Currency;
 public class Compra {
+    private final Cliente cliente;
+    private int idEvento;
+    final ArrayList<String> asientos = new ArrayList<String>();
+    private Currency precioFinal;
+    Compra(Cliente cliente) {
+        this.cliente = cliente;
+    }
+    public Cliente getCliente() {
+        return cliente;
+    }
+    public int getIdEvento() {
+        return idEvento;
+    }
+    public ArrayList<String> getAsientos() {
+        return asientos;
+    }
+    public Currency getPrecioFinal() {
+        return precioFinal;
+    }
+    public boolean pagar(int CVV){
+        return true;
+    }
 }
