f3915ccf013442154417f421defe70dc6b951a09
Alejandro Valenzuela <alejandrovalenzuela051@gmail.com>
2023-05-07 21:09:45 -0600

    Metodos de metodos de pago creados

--- src/main/java/com/megaboletos/usuarios/builders/Cliente.java
+++ src/main/java/com/megaboletos/usuarios/builders/Cliente.java
@@ -5,4 +5,10 @@ class Cliente extends Usuario{
     Cliente(Connection conexion, String correo, String claveAcceso) {
         super(conexion, correo, claveAcceso);
     }
+    public boolean modificarMetodoPago(int idMetodoPago) {
+        return false;
+    }
+    public boolean eliminarMetodoPago(int idMetodoPago) {
+        return false;
+    }
 }
