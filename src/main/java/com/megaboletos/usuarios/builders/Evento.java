dc192f4606c4f698aa3787c6c10e022da2723975
Alejandro Valenzuela <alejandrovalenzuela051@gmail.com>
2023-05-08 00:38:29 -0600

    agregado conexion a Evento

--- src/main/java/com/megaboletos/usuarios/builders/Evento.java
+++ src/main/java/com/megaboletos/usuarios/builders/Evento.java
@@ -1,5 +1,7 @@
 package com.megaboletos.usuarios.builders;
 import com.megaboletos.usuarios.PermisoUsuario;
+
+import java.sql.Connection;
 import java.time.LocalTime;
 import java.util.Date;
 public class Evento {
@@ -7,13 +9,16 @@ public class Evento {
     final private String lugar;
     final private Date fecha;
     final private LocalTime hora;
+    final private Connection conexion;
     private Evento(Builder instancia) {
         this.nombre = instancia.nombre;
         this.lugar = instancia.lugar;
         this.fecha = instancia.fecha;
         this.hora = instancia.hora;
+        this.conexion = instancia.conexion;
     }
-    public Evento(int idEvento){
+    public Evento(Connection conexion, int idEvento){
+        this.conexion = conexion;
     }
     public String getNombre() {
         return nombre;
@@ -32,6 +37,7 @@ public class Evento {
         private String lugar;
         private Date fecha;
         private LocalTime hora;
+        private Connection conexion;
         public Builder(PermisoUsuario permisoUsuario) {
         }
         public void setNombre(String nombre) {
@@ -45,6 +51,9 @@ public class Evento {
         }
         public void setHora(LocalTime hora) {
             this.hora = hora;
+        }
+        public void setConexion(Connection conexion) {
+            this.conexion = conexion;
         }
         public Evento crear() {
             return new Evento(this);
