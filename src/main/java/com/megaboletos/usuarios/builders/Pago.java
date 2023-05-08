54584683b38344e8fc17a32b42aa1b4e173cc841
Alejandro Valenzuela <alejandrovalenzuela051@gmail.com>
2023-05-08 13:58:35 -0600

    creados constructores metodos de pago

--- src/main/java/com/megaboletos/usuarios/builders/Pago.java
+++ src/main/java/com/megaboletos/usuarios/builders/Pago.java
@@ -1,12 +1,16 @@
 package com.megaboletos.usuarios.builders;
 import java.sql.Connection;
 class Visa implements MetodoPago {
+    Visa(Cliente clienteAComprar) {
+    }
     @Override
     public boolean pagar(int cantidadDinero, int CVV) {
         return false;
     }
 }
 class Mastercard implements MetodoPago {
+    Mastercard(Cliente clienteAComprar) {
+    }
     @Override
     public boolean pagar(int cantidadDinero, int CVV) {
         return false;
