2aef05e748010f7ef91b485e8ac482d214950557
Alejandro Valenzuela <alejandrovalenzuela051@gmail.com>
2023-05-08 23:32:38 -0600

    creadas clases mastecard y visa

--- src/main/java/com/megaboletos/pagos/Mastercard.java
+++ src/main/java/com/megaboletos/pagos/Mastercard.java
@@ -2,15 +2,7 @@ package com.megaboletos.pagos;
 
 import com.megaboletos.usuarios.Cliente;
 
-class Visa implements MetodoPago {
-    Visa(Cliente clienteAComprar) {
-    }
-    @Override
-    public boolean pagar(int cantidadDinero, int CVV) {
-        return false;
-    }
-}
-class Mastercard implements MetodoPago {
+public class Mastercard implements MetodoPago{
     Mastercard(Cliente clienteAComprar) {
     }
     @Override
---
+++ src/main/java/com/megaboletos/pagos/Visa.java
@@ -0,0 +1,12 @@
+package com.megaboletos.pagos;
+
+import com.megaboletos.usuarios.Cliente;
+
+public class Visa implements MetodoPago{
+    Visa(Cliente clienteAComprar) {
+    }
+    @Override
+    public boolean pagar(int cantidadDinero, int CVV) {
+        return false;
+    }
+}
