a5eed12674e9ff389a745499b3a1ac3a3a8be3a7
Alejandro Valenzuela <alejandrovalenzuela051@gmail.com>
2023-05-09 01:06:11 -0600

    pares creados para string

--- src/main/java/com/megaboletos/usuarios/Cliente.java
+++ src/main/java/com/megaboletos/usuarios/Cliente.java
@@ -1,11 +1,18 @@
 package com.megaboletos.usuarios;
 import com.megaboletos.ObjetoBase;
 import org.json.JSONObject;
-
 import java.sql.Connection;
 import java.sql.SQLException;
-
+import java.util.HashMap;
+import java.util.Map;
+import static java.util.Map.entry;
 public class Cliente extends Usuario implements ObjetoBase {
+    private Map<String, String> campos = Map.ofEntries(
+            entry("nombre", "str"),
+            entry("apellidopaterno", "str"),
+            entry("correo", "str"),
+            entry("claveiniciosesion", "str")
+    );
     private Cliente(final Builder instancia){
         super(instancia.conexion);
         this.nombre = instancia.nombre;
