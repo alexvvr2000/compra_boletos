7b44425d780fbad3073b173bd23cc7c6e2964fd1
Alejandro Valenzuela <alejandrovalenzuela051@gmail.com>
2023-05-09 11:55:15 -0600

    checar si existe usa sql

--- src/main/java/com/megaboletos/usuarios/Administrador.java
+++ src/main/java/com/megaboletos/usuarios/Administrador.java
@@ -2,26 +2,28 @@ package com.megaboletos.usuarios;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.sql.SQLException;
 
 public class Administrador extends Usuario{
     Administrador(Connection conexion, String correo, String claveAcceso) throws Exception{
         super(conexion, correo, claveAcceso);
     }
-    public static boolean existeUsuario(Connection conexion, int idUsuario) throws SQLException {
+    public static boolean existeUsuario(Connection conexion, int idUsuario) throws Exception {
         PreparedStatement query = conexion.prepareStatement(
-                "select nombre from usuario where idusuario = ?"
+                "select exists(" +
+                        "select nombre from usuario where idusuario = ?" +
+                ") as existe;"
         );
         query.setInt(1, idUsuario);
         ResultSet conjunto = query.executeQuery();
-        return conjunto.next();
+        conjunto.next();
+        return conjunto.getBoolean("existe");
     }
     public static boolean esAdmin(Connection conexion, int idUsuario) throws Exception {
         if(!Administrador.existeUsuario(conexion, idUsuario)) {
             throw new Exception("No existe en base");
         }
         PreparedStatement query = conexion.prepareStatement(
-                "select esadmin from usuario where idusuario = ?"
+                "select esadmin from usuario where idusuario = ?;"
         );
         query.setInt(1, idUsuario);
         ResultSet conjunto = query.executeQuery();
